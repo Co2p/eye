20f296ca042fe8c0c719ea7a428cf4677e7bfad5
Co2p <go.co2p@gmail.com>
2017-04-29 14:16:37 +0200

    eye size and squint

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var oldstamp = 0;
-var irisSize = 300;
+var irisSize = 500;
 var customColor = false;
 var defaultColor = "#1ba254";
 var eyeanim = false;
+var squint = false;
 
 $("body").on("mousemove", (event) => {
   let x = event.pageX;
@@ -26,12 +27,11 @@ $("body").on("scroll touchmove mousewheel", (event) => {
     let x = event.pageX;
     let y = event.pageY;
     $('body').css('cursor', 'none');
-    $( "#iris" ).animate({"height" : irisSize + "px", "width": irisSize + "px", "top": y - (irisSize/2), "left": x - (irisSize/2)}, 10);
+    $( "#iris" ).animate({"height" : irisSize + "px", "width": irisSize + "px", "top": y - (irisSize/2), "left": x - (irisSize/2)}, 10, 'linear');
   }
 });
 
 $("body").on("mousedown", (event) => {
-
   $( "#lock1" ).animate({"height" : 55 + "%"}, 500);
   $( "#lock2" ).animate({"height" : 45 + "%"}, 500);
 });
@@ -42,23 +42,31 @@ $("body").on("click", (event) => {
     $( "#lock1" ).animate({"height" : 55 + "%"}, 5).animate({"height" : 10 + "%"}, 300);
     $( "#lock2" ).animate({"height" : 45 + "%"}, 5).animate({"height" : 10 + "%"}, 300).after(() => {
       eyeanim = false;
-      console.log('hi');
     });
     $( "#iris" ).animate({"height" : irisSize, "width": irisSize}, 1200);
     $("#blackness").animate({"opacity" : 0}, 300);
   }
-
 });
 
 $("body").on("keydown", (event) => {
   if (event.key === 'r') {
     window.location.reload();
+  } else if (event.key === 's') {
+    if (squint) {
+      $( "#lock1" ).animate({"height" : 10 + "%"}, 300);
+      $( "#lock2" ).animate({"height" : 10 + "%"}, 300);
+      squint = false;
+    } else {
+      $( "#lock1" ).animate({"height" : 45 + "%"}, 500);
+      $( "#lock2" ).animate({"height" : 35 + "%"}, 500);
+      squint = true;
+    }
   } else if (!customColor) {
     customColor = true;
     if (event.key === '1') {
       $("body").css("background", "#a71c1c");
     } else if (event.key === '2') {
-      $("body").css("background", "##f98484");
+      $("body").css("background", "#f98484");
     }
   } else {
     customColor = false;
