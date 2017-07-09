620a93525bbe6d4f9a629aadfd7dd08431b2d6dd
Co2p <go.co2p@gmail.com>
2017-07-09 17:05:16 +0200

    Multitouch and more ES6

--- app.js
+++ app.js
@@ -1,21 +1,84 @@
-var oldstamp = 0;
-var irisSize = 500;
-var customColor = false;
-var defaultColor = "#1ba254";
-var eyeanim = false;
-var squint = false;
-var squinttop = false;
-var squintbot = false;
+let oldstamp = 0;
+let irisSize = 500;
+let customColor = false;
+let defaultColor = "#1ba254";
+let eyeanim = false;
+let squint = false;
+let squinttop = false;
+let squintbot = false;
 let body = $('body');
 let iris = $('#iris');
 let lock1 = $('#lock1');
 let lock2 = $('#lock2');
 
-window.addEventListener("mousemove", (event) => {
-  let x = event.pageX;
-  let y = event.pageY;
+var manager = new Hammer.Manager(document.body);
+
+// create recognizers
+var Pan = new Hammer.Pan();
+var Pinch = new Hammer.Pinch();
+
+var Tap = new Hammer.Tap({
+  taps: 1
+});
+var DoubleTap = new Hammer.Tap({
+  event: 'doubletap',
+  taps: 2
+});
+
+// use them together
+Rotate.recognizeWith([Pan]);
+Pinch.recognizeWith([Pan]);
+Pan.recognizeWith([Pinch])
+
+DoubleTap.recognizeWith([Tap]);
+Tap.requireFailure([DoubleTap]);
+
+// add the recognizers
+manager.add(Pan);
+manager.add(Pinch);
+manager.add(DoubleTap);
+manager.add(Tap);
+
+// subscribe to events
+var liveScale = 1;
+var currentRotation = 0;
+
+var deltaX = 0;
+var deltaY = 0;
+manager.on('panmove', (e) => {
+  // do something cool
+  var dX = deltaX + (e.deltaX);
+  var dY = deltaY + (e.deltaY);
+  $.Velocity.hook(iris, 'translateX', dX + 'px');
+  $.Velocity.hook(iris, 'translateY', dY + 'px');
+});
+manager.on('panend', function(e) {
+  deltaX = deltaX + e.deltaX;
+  deltaY = deltaY + e.deltaY;
+});
+
+// subscribe to events
+var currentScale = 1;
+function getRelativeScale(scale) {
+  return scale * currentScale;
+}
+manager.on('pinchmove', function(e) {
+  // do something cool
+  var scale = getRelativeScale(e.scale);
+  $.Velocity.hook(iris, 'scale', scale);
+});
+manager.on('pinchend', function(e) {
+  // cache the scale
+  currentScale = getRelativeScale(e.scale);
+  liveScale = currentScale;
+});
+
+window.addEventListener("mousemove", (e) => {
   body.css({'cursor': 'none'});
-  iris.css({ 'top':y - (irisSize/2), 'left': x - (irisSize/2)});
+  var dX = e.screenX - irisSize/2;
+  var dY = e.screenY - irisSize/2;
+  $.Velocity.hook(iris, 'left', dX + 'px');
+  $.Velocity.hook(iris, 'top', dY + 'px');
 });
 
 window.addEventListener("scroll touchmove mousewheel", (event) => {
